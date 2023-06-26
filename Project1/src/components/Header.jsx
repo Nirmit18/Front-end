8ffae6507f9b098aa4410f0b71df80569fdde80d
Nirmit18 <114016215+Nirmit18@users.noreply.github.com>
2023-06-26 23:32:14 +0530

    Header.jsx

---
+++ Project1/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav>
+        <h1>Techstar</h1>
+        <main>
+        
+        </main>
+    </nav>
+  )
+};
+
+export default Header;
